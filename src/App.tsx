9ea06845c1fd8a42753cd6dfc1cec009ca17ad0e
Milner <cmilner@datto.com>
2023-11-23 15:49:57 +0000

    playing around with things

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      <nav>
+        <a href={'#'}>Home</a>
+        <a href={'#'}>About</a>
+        <a href={'#'}>Experience</a>
+        <a href={'#'}>Contact</a>
+      </nav>
       <header className="App-header">
         <section>
-          <h2>Welcome to cmil.me</h2>
-          <p>
-            <code>Built by &lt;Clint /&gt;</code>
-          </p>
+          <h1>Hello</h1>
+          {/*<h2>Welcome to cmil.me</h2>*/}
+          <p>I'm Clint Milner. Web Developer.</p>
+          {/*<p>*/}
+          {/*  <code>Built by &lt;Clint /&gt;</code>*/}
+          {/*</p>*/}
         </section>
       </header>
     </div>
